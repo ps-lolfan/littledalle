09f3ccc6d7c9680181ea9447b1ba3d91210822d8
ps-lolfan <pbsinnur@gmail.com>
2023-03-08 10:41:21 +0530

    app correction

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+
+  const handleSubmit = () => {
+
+  }
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+          Create
+        </h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w[500px]">
+          Create imaginative and visually stunning
+          images through DALL-E AI and share them with the community
+        </p>
+      </div>
+
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+
+      </form>
+    </section>
+  )
 };
 
 export default CreatePost;
